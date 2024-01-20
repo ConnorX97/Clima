1c972512064f92f211175f086a6e2f03a225edb0
Sherzod <fayziyev658@gmail.com>
2024-01-20 13:19:39 +0500

    Added session tasks

--- Clima/Models/WeatherManager.swift
+++ Clima/Models/WeatherManager.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e000bff636b7c9666d77e9d1d8e909d3&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+        print (urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        //1.Create URL
+        if let url = URL(string: urlString) {
+            
+            //2.Create URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3.Give the session task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print (error!)
+                    return
+                }
+                if let safeData = data {
+                    
+                }
+            }
+            //4.Start the task
+            task.resume()
+        }
+    }
+}
